a2a48b33408f174f59219c2fd659ad4bdb71a749
Gyebro <gyebroszkigergely@gmail.com>
2023-12-07 18:57:36 +0100

    Added Day05 part two (brute force)

--- sources/day05.cpp
+++ sources/day05.cpp
@@ -1,4 +1,5 @@
 #include <numeric>
+#include <limits>
 #include "day05.h"
 
 Day05::Day05(const string& inputpath) : path(inputpath) { }
@@ -12,7 +13,7 @@ struct MapInterval {
 class Day05Map {
 public:
     vector<MapInterval> intervals;
-    size_t apply(const size_t value) {
+    size_t apply(const size_t value) const {
         for (const MapInterval& i : intervals) {
             if ((value >= i.source && value < (i.source+i.range_length))) {
                 return (i.destination + value - i.source);
@@ -29,6 +30,20 @@ const string map_d_header = "water-to-light";
 const string map_e_header = "light-to-temperature";
 const string map_f_header = "temperature-to-humidity";
 const string map_g_header = "humidity-to-location";
+
+size_t get_lowest_location(const vector<Day05Map>& maps, const MapInterval& seed_interval) {
+    size_t lowest = numeric_limits<size_t>::max();
+    for (size_t s = seed_interval.source; s < seed_interval.source+seed_interval.range_length; s++) {
+        size_t loc = s;
+        for (const auto & map : maps) {
+            loc = map.apply(loc);
+        }
+        if (loc < lowest) {
+            lowest = loc;
+        }
+    }
+    return lowest;
+}
 
 int Day05::solve(bool part_one) const {
     string line;
@@ -73,27 +88,54 @@ int Day05::solve(bool part_one) const {
         }
         infile.close();
     }
-    vector<size_t> transformed;
+    size_t min_loc = 0;
     if (part_one) {
+        vector<size_t> transformed;
         for (int m = 0; m < maps.size(); m++) {
             transformed.clear();
             for (const size_t& s : seeds) {
                 size_t t = maps[m].apply(s);
                 transformed.push_back(t);
             }
-            /*for (const size_t& v : transformed) {
-                cout << v << " ";
-            }
-            cout << endl;*/
             seeds = transformed;
         }
+        min_loc = *min_element(transformed.begin(), transformed.end());
+    } else {
+        // First guess for a working algorithm:
+        //  find the lowest 2-3 intervals of the last map (including the identity map of range 0-N) and
+        //  work backwards. Then check intersection with seed ranges
+        // But first: brute force this
+
+        // Gather seed intervals
+        vector<MapInterval> seed_ints;
+        for (size_t i=0; i<seeds.size(); i+=2) {
+            MapInterval inter;
+            inter.source = seeds[i];
+            inter.range_length = seeds[i + 1]/2;
+            seed_ints.push_back(inter);
+            inter.source = seeds[i]+seeds[i + 1]/2-1;
+            inter.range_length = seeds[i + 1]/2;
+            seed_ints.push_back(inter);
+        }
+        vector<size_t> lowest_in_blocks;
+#pragma omp parallel
+        {
+            vector<size_t> lowest_private;
+#pragma omp for nowait
+            for (const MapInterval& inter : seed_ints) {
+                lowest_private.push_back(get_lowest_location(maps, inter));
+            }
+#pragma omp critical
+            lowest_in_blocks.insert(lowest_in_blocks.end(), lowest_private.begin(), lowest_private.end());
+        }
+        min_loc = *min_element(lowest_in_blocks.begin(), lowest_in_blocks.end());
     }
-    return *min_element(seeds.begin(), seeds.end());
+    return min_loc;
 }
 
 int main() {
     //Day05 day05("test05.txt");
     Day05 day05("day05.txt");
-    cout << "Part one: " << day05.solve() << endl;
+    cout << "Part two: " << endl << day05.solve(false) << endl;
     return 0;
 }
